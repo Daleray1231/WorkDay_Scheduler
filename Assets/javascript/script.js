84c6ae775869c5267aa999fa485f7ab0cf1c9bdb
Daleray1231 <daleray1231@gmail.com>
2023-09-12 18:05:44 -0500

    updated README file

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,39 +1,59 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  // Function to update time-block classes based on the current time.
+  function updateTimeBlockClasses() {
+    // Get the current hour using Day.js in 24-hour format.
+    var currentHour = dayjs().hour();
+
+    // Loop through each time-block.
+    $(".time-block").each(function () {
+      // Get the hour from the time-block's id (e.g., "hour-9" => 9).
+      var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // Remove existing classes to reset the styling.
+      $(this).removeClass("past present future");
+
+      // Compare the time block's hour to the current hour and apply appropriate class.
+      if (timeBlockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (timeBlockHour === currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  // Function to load saved user input from local storage.
+  function loadSavedUserInput() {
+    // Loop through each time-block.
+    $(".time-block").each(function () {
+      // Get the user input saved in local storage for this time block.
+      var savedUserInput = localStorage.getItem($(this).attr("id"));
+
+      // Set the value of the corresponding textarea.
+      $(this).find(".description").val(savedUserInput);
+    });
+  }
+
+  // Update time-block classes initially.
+  updateTimeBlockClasses();
+
+  // Load saved user input initially.
+  loadSavedUserInput();
+
+  // Add a click event listener for the save buttons.
+  $(".saveBtn").on("click", function () {
+    // Get the id of the parent time-block.
+    var timeBlockId = $(this).closest(".time-block").attr("id");
+
+    // Get the user input from the corresponding textarea.
+    var userInput = $(this).siblings(".description").val();
+
+    // Save the user input in local storage with the time block id as the key.
+    localStorage.setItem(timeBlockId, userInput);
+  });
+
+  // Display the current date in the header of the page.
+  var currentDate = dayjs().format("dddd, MMMM D, YYYY");
+  $("#currentDay").text("Today is " + currentDate);
 });
-
-// TODO: Add code to display the current date in the header of the page.
-
-// Get the current date
-var currentDate = new Date();
-
-// Create an array of day names
-var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-// Get the current day of the week (0 = Sunday, 1 = Monday, etc.)
-var dayOfWeek = currentDate.getDay();
-
-// Get the current date in a human-readable format
-var date = currentDate.toLocaleDateString();
-
-// Display the current day and date in the "currentDateTime" div
-document.getElementById("currentDay").textContent = "Today is " + daysOfWeek[dayOfWeek] + ", " + date;
--- README.md
+++ README.md
@@ -1,33 +1,28 @@
-# User Story
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
+# Work Day Scheduler Readme
 
+## Overview
+The Work Day Scheduler is a simple web application that allows users to schedule their work day by entering tasks or events for each hour. It provides a visual representation of past, present, and future time blocks to help users manage their tasks effectively.
 
-# Acceptance Criteria
-GIVEN I am using a daily planner to create a schedule
+## Features
+1. **Time Block Styling**
+   - The application uses Bootstrap and CSS classes to style time blocks based on the current time.
+   - Past time blocks are displayed with a gray background.
+   - The present time block is displayed with a red background.
+   - Future time blocks are displayed with a green background.
 
-WHEN I open the planner  
-THEN the current day is displayed at the top of the calendar  
+2. **Current Date Display**
+   - The current date is dynamically displayed in the header using Day.js.
 
-WHEN I scroll down  
-THEN I am presented with time blocks for standard business hours of 9am to 5pm   
-  
-WHEN I view the time blocks for that day  
-THEN each time block is color-coded to indicate whether it is in the past, present, or future  
+3. **User Input and Local Storage**
+   - Users can enter their tasks or events for each time block in the provided text areas.
+   - Clicking the save button for a time block saves the user's input in local storage with the time block's id as the key.
+   - When the page is refreshed, the saved user input is loaded from local storage and displayed in the corresponding time blocks.
 
-WHEN I click into a time block  
-THEN I can enter an event  
+## Technologies Used
+- HTML
+- CSS (Bootstrap)
+- JavaScript (jQuery and Day.js)
+- Local Storage
 
-WHEN I click the save button for that time block  
-THEN the text for that event is saved in local storage  
-
-WHEN I refresh the page  
-THEN the saved events persist  
-
-# Work Day Scheduler
-
-# Sources
-Starter Code: https://github.com/coding-boot-camp/crispy-octo-meme
-
-![Alt text](image.png)
+## Installation
+Clone this repository to your local machine:
